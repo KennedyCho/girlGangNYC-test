36614b1a7a8396749d0a78494a4ff7fca2a744ed
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2021-02-01 13:34:10 -1000

    add AWS Amplify

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// AWS Amplify 
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
+Amplify.configure(config)
+
 
 export default function App() {
   return (
